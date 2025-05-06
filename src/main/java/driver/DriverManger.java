b24a00d246c979f94d806f304540829df0585376
mohamed-kabeel <111576790+mohamed-kabeel@users.noreply.github.com>
2025-05-06 04:28:48 +0300

    Update DriverManger.java

--- src/main/java/driver/DriverManger.java
+++ src/main/java/driver/DriverManger.java
@@ -32,45 +32,6 @@ public class DriverManger {
 
         return threadLocalDriver.get();
     }
-    static FirefoxOptions setFireFoxOption(){
-        FirefoxOptions options =  new FirefoxOptions();
-        List<String> firefoxOptions = new ArrayList<>();
-        firefoxOptions.add("--headless");
-        firefoxOptions.add("--disable-gpu");
-        firefoxOptions.add("--window-size=1920,1080");
-        firefoxOptions.add("--ignore-certificate-errors");
-        firefoxOptions.add("--start-maximized");
-        firefoxOptions.add("--disable-infobars");
-        firefoxOptions.add("--disable-dev-shm-usage");
-        firefoxOptions.add("--no-sandbox");
-        firefoxOptions.add("--disable-extensions");
-        firefoxOptions.add("--disable-dev-shm-usage");
-        firefoxOptions.add("--no-sandbox");
-        firefoxOptions.add("--disable-extensions");
-        options.addArguments();
-
-        return options;
-    }
-    static ChromeOptions setChromeOption(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--ignore-certificate-errors");
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        return options;
-    }
-
-    static EdgeOptions setEdgeOption() {
-        EdgeOptions options = new EdgeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--start-maximized");
-        return options;
-
-    }
+  
 
 }
